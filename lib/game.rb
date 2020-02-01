51bb164e5c722853219c4ef306692eec9cec7b22
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-02-01 15:58:06 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -28,7 +28,6 @@ class Game
         if @board.cells[combination[0]] == @board.cells[combination[1]] &&
           @board.cells[combination[1]] == @board.cells[combination[2]] &&
           @board.taken?(combination[0]+1)
-          #Need to +1, because #taken? rspec test is working off user_input range (1-9)
           return combination
         end
       end
@@ -74,4 +73,3 @@ class Game
 
 
 end
-© 2020 GitHub, Inc.
